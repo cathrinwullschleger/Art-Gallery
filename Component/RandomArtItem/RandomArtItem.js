0b90fcc1c8f97b91daa80e70e549e2fa34ea8dea
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-07-05 22:04:48 +0200

    add styled componennt so style.js

--- Component/RandomArtItem/RandomArtItem.js
+++ Component/RandomArtItem/RandomArtItem.js
@@ -1,22 +1,23 @@
 import Image from "next/image";
 import { ArtPiece } from "@/styles.js";
-
+import { Container } from "@/styles.js";
 export default function RandomArtItem({ artPieces }) {
   const length = artPieces.length;
   const randomIndex = Math.floor(Math.random() * length); // Math.random -> number 0-1 /Math.floor -> round down
   const randomArtPiece = artPieces[randomIndex];
 
   return (
-    // if time left -> styled component
-    <ArtPiece>
-      <Image
-        src={randomArtPiece.imageSource}
-        alt={randomArtPiece.name}
-        width={300}
-        height={200}
-        style={{ objectFit: "cover" }}
-      />
-      <p>{randomArtPiece.artist}</p>
-    </ArtPiece>
+    <Container>
+      <ArtPiece>
+        <Image
+          src={randomArtPiece.imageSource}
+          alt={randomArtPiece.name}
+          width={300}
+          height={200}
+          style={{ objectFit: "cover" }}
+        />
+        <p>{randomArtPiece.artist}</p>
+      </ArtPiece>
+    </Container>
   );
 }
