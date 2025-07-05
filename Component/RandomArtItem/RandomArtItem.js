a9a544e5023b1831681f4b7ec4d6361462631f73
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-07-05 22:14:48 +0200

    fitx bug

--- Component/RandomArtItem/RandomArtItem.js
+++ Component/RandomArtItem/RandomArtItem.js
@@ -3,6 +3,10 @@ import { ArtPiece } from "@/styles.js";
 import { Container } from "@/styles.js";
 export default function RandomArtItem({ artPieces }) {
   const length = artPieces.length;
+  if (!artPieces || artPieces.length === 0) {
+    return null;
+  }
+
   const randomIndex = Math.floor(Math.random() * length); // Math.random -> number 0-1 /Math.floor -> round down
   const randomArtPiece = artPieces[randomIndex];
 
@@ -16,7 +20,7 @@ export default function RandomArtItem({ artPieces }) {
           height={200}
           style={{ objectFit: "cover" }}
         />
-        <p>{artPieces && <RandomArtItem artPieces={artPieces} />}</p>
+        <p>{randomArtPiece.name} </p>
       </ArtPiece>
     </Container>
   );
