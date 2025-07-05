72aafa6d5f5a4b0bbebd32237b357b099ecb2c4f
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-07-05 14:55:28 +0200

    add data fetching with swr on index, implement RandomArtItem Componennt to render a random Art Piece

---
+++ Component/RandomArtItem/RandomArtItem.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function RandomArtItem({ artPieces }) {
+  const length = artPieces.length;
+  const randomIndex = Math.floor(Math.random() * length); // Math.random -> number 0-1 /Math.floor -> round down
+  const randomArtPiece = artPieces[randomIndex];
+
+  return (
+    // if time left -> styled component
+    <>
+      <Image
+        src={randomArtPiece.imageSource}
+        alt={randomArtPiece.name}
+        width={300}
+        height={200}
+        style={{ objectFit: "cover" }}
+      />
+      <p>{randomArtPiece.artist}</p>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+import useSWR from "swr";
+import RandomArtItem from "@/Component/RandomArtItem/RandomArtItem.js";
+
+const URL = "https://example-apis.vercel.app/api/art";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
 export default function HomePage() {
+  const { data: artPieces, error, isLoading } = useSWR(URL, fetcher);
+
+  if (isLoading) return <p>Loading artwork preview ... please wait a moment</p>;
+  if (error)
+    return (
+      <p>
+        Something went wrong while loading the artwork preview. Try refreshing
+        the page.
+      </p>
+    );
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Art Gallery</h1>
+      <RandomArtItem artPieces={artPieces} />
     </div>
   );
 }
