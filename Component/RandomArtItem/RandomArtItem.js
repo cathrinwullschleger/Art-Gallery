d4fb12c25d9464693ab58f3df296957c876cdfe5
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-07-05 22:18:38 +0200

    fix bug

--- Component/RandomArtItem/RandomArtItem.js
+++ Component/RandomArtItem/RandomArtItem.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { ArtPiece } from "@/styles.js";
 import { Container } from "@/styles.js";
 export default function RandomArtItem({ artPieces }) {
-  const length = artPieces.length;
   if (!artPieces || artPieces.length === 0) {
     return null;
   }
+  const length = artPieces.length;
 
   const randomIndex = Math.floor(Math.random() * length); // Math.random -> number 0-1 /Math.floor -> round down
   const randomArtPiece = artPieces[randomIndex];
